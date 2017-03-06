73c84d19379d4b9756ea9740877e5f0358a95acb
krisc457 <kristoffer.schmidt@hotmail.com>
2017-03-06 10:20:30 +0100

    Lite mer kod

--- FizzBuzz/src/com/example/Main.java
+++ FizzBuzz/src/com/example/Main.java
@@ -1,17 +1,28 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ange ett tal");
 
-        for (int i = 0; i <= 100; i++) {
+        while (true) {
+        String s = input.next();
+        if (s == null)
+            break;
+        int i = Integer.parseInt(s);
 
             if ((i % 3 != 0) && (i % 5 != 0)) {
                 System.out.println(i);
             }
             if ((i % 3 == 0) && (i % 5 != 0)) {
                 System.out.println("Fizz");
+            }
+            if ((i % 3 != 0) && (i % 5 == 0)) {
+                System.out.println("Buzz");
             }
             if ((i % 3 == 0) && (i % 5 == 0)) {
                 System.out.println("FizzBuzz");
